a2464cf233b1b736363b04295f1009f81116ce90
Moath-design <138505320+Moath-design@users.noreply.github.com>
2024-03-18 13:26:37 +0000

    Create Compos.

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import './App.css';
+import Header from './components/header';
+import Footer from './components/footer';
+import Home from './components/home';
+import About from './components/about';
+
+
+function App() {
+  return (
+    <Router>
+      <Header/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/about' element={<About/>}/>
+      </Routes>
+      <Footer/>
+    </Router>
+    
+  );
+}
+
+export default App;
